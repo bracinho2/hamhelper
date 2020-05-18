4d96889766a69c69cbcb9d1fa32bc2fd12f38a78
bracinho2 <bracinho2@hotmail.com>
2020-05-18 20:19:02 -0300

    Update player class.

--- lib/player.dart
+++ lib/player.dart
@@ -1,7 +1,32 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-class Player {
-  AudioPlayer advancedPlayer = new AudioPlayer();
-  AudioCache audioCache = new AudioCache(fixedPlayer: advancedPlayer);
+class Player extends StatefulWidget {
+  @override
+  _PlayerAudio createState() =>  _PlayerAudio();
 }
+
+class _Player extends State<Player> {
+  Duration _duration = new Duration();
+  Duration _position = new Duration();
+  AudioPlayer advancedPlayer;
+  AudioCache audioCache;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlayer();
+  }
+
+  void initPlayer() {
+    advancedPlayer = new AudioPlayer();
+    audioCache = new AudioCache(fixedPlayer: advancedPlayer);
+
+    advancedPlayer.durationHandler = (d) => setState(() {
+      _duration = d;
+    });
+
+    advancedPlayer.positionHandler = (p) => setState(() {
+      _position = p;
+    });
+  }
